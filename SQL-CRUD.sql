8ffecf34cae772761d3d5b543c27da944584bb21
Gergana Ivanova <geri7423@abv.bg>
2025-01-28 18:33:07 +0200

    Update SQL-CRUD.sql

--- SQL-CRUD.sql
+++ SQL-CRUD.sql
@@ -91,3 +91,14 @@ CREATE
 	               [Salary]
 			  FROM [Employees]
 		     )
+
+--Task 17
+CREATE 
+  VIEW [V_EmployeeNameJobTitle]
+         AS (
+			SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName])
+	                   AS [Full Name],
+				          [JobTitle]
+				       AS [Job Title]
+			  FROM [Employees]
+		     )
