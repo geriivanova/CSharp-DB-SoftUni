09e74e39f434b379884d45f9ac8f12f45486ee4e
Gergana Ivanova <geri7423@abv.bg>
2025-01-28 19:01:58 +0200

    Update SQL-CRUD.sql

--- SQL-CRUD.sql
+++ SQL-CRUD.sql
@@ -138,3 +138,11 @@ UPDATE [Employees]
   SELECT [PeakName]
     FROM [Peaks]
 ORDER BY [PeakName] ASC
+
+--Task 23
+  SELECT TOP (30)[CountryName],
+	             [Population]
+        FROM [Countries] 
+	   WHERE [ContinentCode] = 'EU'
+    ORDER BY [Population] DESC,
+             [CountryName] ASC
