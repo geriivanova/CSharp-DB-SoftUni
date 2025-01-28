e2a766412f12675c5ee70aabf281f1e0f4a79da6
Gergana Ivanova <geri7423@abv.bg>
2025-01-28 19:11:12 +0200

    Update SQL-CRUD.sql

--- SQL-CRUD.sql
+++ SQL-CRUD.sql
@@ -146,3 +146,14 @@ ORDER BY [PeakName] ASC
 	   WHERE [ContinentCode] = 'EU'
     ORDER BY [Population] DESC,
              [CountryName] ASC
+
+--Task 24
+  SELECT [CountryName],
+         [CountryCode],
+	CASE 
+		WHEN [CurrencyCode] = 'EUR' THEN 'Euro'
+		ELSE 'Not Euro'
+	 END
+	  AS [Currency]
+    FROM [Countries]
+ORDER BY [CountryName]
