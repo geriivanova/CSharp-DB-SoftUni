c553416b423a35646d44122b80c2b34ba71374d9
Gergana Ivanova <geri7423@abv.bg>
2025-01-28 17:59:47 +0200

    Update SQL-CRUD.sql

--- SQL-CRUD.sql
+++ SQL-CRUD.sql
@@ -34,3 +34,10 @@ SELECT DISTINCT [Salary]
 SELECT * 
   FROM [Employees]
  WHERE [JobTitle] = 'Sales Representative'
+
+--Task 9
+SELECT [FirstName], 
+	   [LastName],
+	   [JobTitle]
+  FROM [Employees]
+ WHERE [Salary] BETWEEN 20000 AND 30000
