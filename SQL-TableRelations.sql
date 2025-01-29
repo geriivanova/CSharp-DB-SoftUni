f2f337dcf132cf15acae1f45bbe018b1067668c7
Gergana Ivanova <geri7423@abv.bg>
2025-01-29 18:19:25 +0200

    Update SQL-TableRelations.sql

--- SQL-TableRelations.sql
+++ SQL-TableRelations.sql
@@ -47,3 +47,30 @@ ADD CONSTRAINT FK_Persons_Passports
 
    ALTER TABLE [Persons]
 ADD CONSTRAINT UQ_Person_PassportID UNIQUE (PassportID)
+
+--Task 2
+
+CREATE TABLE [Manufacturers](
+	[ManufacturerID] INT PRIMARY KEY,
+	[Name] VARCHAR(64) NOT NULL,
+	[EstablishedOn] DATETIME
+)
+
+CREATE TABLE [Models](
+	[ModelID] INT PRIMARY KEY,
+	[Name] VARCHAR(64) NOT NULL,
+	[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID])
+)
+
+INSERT INTO [Manufacturers]
+     VALUES (1, 'BMW', 07/03/1916),
+	        (2, 'Tesla', 01/01/2003),
+			(3, 'Lada', 01/05/1966)
+
+INSERT INTO [Models]
+     VALUES (101, 'X1', 1),
+	        (102, 'i6', 1),
+			(103, 'Model S', 2),	
+			(104, 'Model X', 2),
+			(105, 'Model 3', 2),
+			(106, 'Nova', 3)
