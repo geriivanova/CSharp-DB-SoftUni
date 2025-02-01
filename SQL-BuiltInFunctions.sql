bf4183e7fc32bc90f5a6702281f017e1b69d03f8
Gergana Ivanova <geri7423@abv.bg>
2025-02-01 21:04:14 +0200

    Update SQL-BuiltInFunctions.sql

--- SQL-BuiltInFunctions.sql
+++ SQL-BuiltInFunctions.sql
@@ -43,3 +43,13 @@ SELECT [TownID],
     FROM [Towns]
    WHERE LEFT([Name], 1) NOT IN ('R', 'B', 'D')
 ORDER BY [Name]
+
+--Task 8
+CREATE VIEW [V_EmployeesHiredAfter2000]
+         AS 
+	 SELECT [FirstName],
+		    [LastName]
+	   FROM [Employees]
+	  WHERE DATEPART(YEAR, [HireDate]) > 2000
+	
+SELECT * FROM [V_EmployeesHiredAfter2000]
