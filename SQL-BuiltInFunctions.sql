96b283ef6457daaf80b8f4e20ebe66b4919ba9a2
Gergana Ivanova <geri7423@abv.bg>
2025-02-01 21:05:34 +0200

    Update SQL-BuiltInFunctions.sql

--- SQL-BuiltInFunctions.sql
+++ SQL-BuiltInFunctions.sql
@@ -53,3 +53,9 @@ CREATE VIEW [V_EmployeesHiredAfter2000]
 	  WHERE DATEPART(YEAR, [HireDate]) > 2000
 	
 SELECT * FROM [V_EmployeesHiredAfter2000]
+
+--Task 9
+SELECT [FirstName],
+	   [LastName]
+  FROM [Employees]
+ WHERE LEN([LastName]) = 5
