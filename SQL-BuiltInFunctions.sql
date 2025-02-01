26d31a35ce8d92fc97fb9c9b166916c27e78bc68
Gergana Ivanova <geri7423@abv.bg>
2025-02-01 20:53:39 +0200

    Update SQL-BuiltInFunctions.sql

--- SQL-BuiltInFunctions.sql
+++ SQL-BuiltInFunctions.sql
@@ -29,3 +29,10 @@ SELECT [FirstName],
     FROM [Towns]
    WHERE LEN([Name]) IN (5, 6)
 ORDER BY [Name]
+
+--Task 6
+  SELECT [TownID], 
+         [Name]
+    FROM [Towns]
+   WHERE LEFT([Name], 1) IN ('M', 'K', 'B', 'E')
+ORDER BY [Name]
