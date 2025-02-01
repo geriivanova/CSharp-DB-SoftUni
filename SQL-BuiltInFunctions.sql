eb0042667583bc36d00edf07c02d560d3f99c442
Gergana Ivanova <geri7423@abv.bg>
2025-02-01 18:08:39 +0200

    Update SQL-BuiltInFunctions.sql

--- SQL-BuiltInFunctions.sql
+++ SQL-BuiltInFunctions.sql
@@ -5,3 +5,9 @@ SELECT [FirstName],
        [LastName]
   FROM [Employees]
  WHERE [FirstName] LIKE 'Sa%'
+
+--Task 2
+SELECT [FirstName],
+       [LastName]
+  FROM [Employees]
+ WHERE [LastName] LIKE '%ei%'
