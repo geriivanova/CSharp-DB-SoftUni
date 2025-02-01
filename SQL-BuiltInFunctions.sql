e38cd611cc22fcbea9477ca4fb64bb1498866db2
Gergana Ivanova <geri7423@abv.bg>
2025-02-01 21:24:14 +0200

    Update SQL-BuiltInFunctions.sql

--- SQL-BuiltInFunctions.sql
+++ SQL-BuiltInFunctions.sql
@@ -70,3 +70,18 @@ SELECT [FirstName],
     FROM [Employees]
    WHERE [Salary] BETWEEN 10000 AND 50000
 ORDER BY [Salary] DESC
+
+--Task 11
+ SELECT [D].*
+   FROM (
+           SELECT [EmployeeID],
+                  [FirstName],
+	              [LastName],
+	              [Salary],
+	     DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID])
+	           AS [Rank]
+             FROM [Employees] 
+            WHERE [Salary] BETWEEN 10000 AND 50000
+         ) AS [D]
+   WHERE [Rank] = 2
+ORDER BY [Salary] DESC
