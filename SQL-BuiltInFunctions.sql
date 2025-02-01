6870f372184a77c9064beb5e125abf7e38efe45f
Gergana Ivanova <geri7423@abv.bg>
2025-02-01 21:13:18 +0200

    Update SQL-BuiltInFunctions.sql

--- SQL-BuiltInFunctions.sql
+++ SQL-BuiltInFunctions.sql
@@ -59,3 +59,14 @@ SELECT [FirstName],
 	   [LastName]
   FROM [Employees]
  WHERE LEN([LastName]) = 5
+
+--Task 10
+  SELECT [EmployeeID],
+         [FirstName],
+	     [LastName],
+	     [Salary],
+	     DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID])
+	  AS [Rank]
+    FROM [Employees]
+   WHERE [Salary] BETWEEN 10000 AND 50000
+ORDER BY [Salary] DESC
