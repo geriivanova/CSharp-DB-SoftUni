e74647bf3d902c48ca0238d507bae1b15c134cce
Gergana Ivanova <geri7423@abv.bg>
2025-02-02 18:45:19 +0200

    Update SQL-SubqueriesAndJoins.sql

--- SQL-SubqueriesAndJoins.sql
+++ SQL-SubqueriesAndJoins.sql
@@ -217,3 +217,24 @@ LEFT JOIN [Mountains]
        AS [M]
        ON [MC].[MountainId] = [M].[Id]
     WHERE [M].[Id] IS NULL
+
+--Task 17
+   SELECT 
+  TOP (5) [C].[CountryName],
+          MAX([P].[Elevation]) AS [HighestPeakElevation],
+          MAX([R].[Length]) AS [LongestRiverLength]
+     FROM [Countries] AS [C]
+LEFT JOIN [MountainsCountries] AS [MC] 
+       ON [C].[CountryCode] = [MC].[CountryCode]
+LEFT JOIN [Mountains] AS [M] 
+       ON [MC].[MountainId] = [M].[Id]
+LEFT JOIN [CountriesRivers] AS [CR] 
+       ON [C].[CountryCode] = [CR].[CountryCode]
+LEFT JOIN [Rivers] AS [R]
+       ON [CR].[RiverId] = [R].[Id]
+LEFT JOIN [Peaks] AS [P]
+       ON [P].[MountainId] = [M].[Id]
+ GROUP BY [C].[CountryName]
+ ORDER BY [HighestPeakElevation] DESC, 
+          [LongestRiverLength] DESC, 
+          [C].[CountryName] ASC
