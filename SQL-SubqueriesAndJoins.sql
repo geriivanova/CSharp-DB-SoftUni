6cfa5b5e1a1e02c09b526fb41cda47fc8e85fd43
Gergana Ivanova <geri7423@abv.bg>
2025-02-02 12:26:59 +0200

    Update SQL-SubqueriesAndJoins.sql

---
+++ SQL-SubqueriesAndJoins.sql
@@ -0,0 +1,12 @@
+USE [SoftUni]
+
+--Task 1
+    SELECT 
+    TOP(5) [E].[EmployeeID],
+           [E].[JobTitle],
+	       [E].[AddressID],
+	       [A].[AddressText]
+      FROM [Employees] AS [E]
+INNER JOIN [Addresses] AS [A]
+        ON [E].[AddressID] = [A].[AddressID]
+  ORDER BY [E].[AddressID] ASC
