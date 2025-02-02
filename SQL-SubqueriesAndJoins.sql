a4ddc5f80dd7e87c260894b9c8e7c1f5247b1243
Gergana Ivanova <geri7423@abv.bg>
2025-02-02 12:41:40 +0200

    Update SQL-SubqueriesAndJoins.sql

--- SQL-SubqueriesAndJoins.sql
+++ SQL-SubqueriesAndJoins.sql
@@ -24,3 +24,15 @@ INNER JOIN [Towns] AS [T]
         ON [A].[TownID] = [T].[TownID]
   ORDER BY [FirstName] ASC,
            [LastName] ASC
+
+--Task 3
+    SELECT [E].[EmployeeID],
+	       [E].[FirstName],
+		   [E].[LastName],
+		   [D].[Name] AS [DepartmentName]
+      FROM [Employees] AS [E]
+INNER JOIN [Departments] AS [D]
+        ON [E].[DepartmentID] = [D].[DepartmentID]
+     WHERE [D].[Name] = 'Sales'
+  ORDER BY [E].[EmployeeID] ASC
+
