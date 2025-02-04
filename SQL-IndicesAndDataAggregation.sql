e0b9a8bd02f1d96cd097993c4c3e39281f978790
Gergana Ivanova <geri7423@abv.bg>
2025-02-04 16:35:57 +0200

    Update SQL-IndicesAndDataAggregation.sql

--- SQL-IndicesAndDataAggregation.sql
+++ SQL-IndicesAndDataAggregation.sql
@@ -49,3 +49,14 @@ GROUP BY [DepositGroup]
 GROUP BY [DepositGroup]
   HAVING SUM([DepositAmount]) < 150000
 ORDER BY [TotalSum] DESC
+
+--Task 8
+  SELECT [DepositGroup],
+         [MagicWandCreator],
+         MIN([DepositCharge])
+	  AS [MinDepositCharge]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup],
+         [MagicWandCreator]
+ORDER BY [MagicWandCreator] ASC,
+         [DepositGroup] ASC
