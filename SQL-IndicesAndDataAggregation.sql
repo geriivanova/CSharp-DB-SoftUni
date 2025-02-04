960b7ccff18b880a63f1dc37573664c3872e7e6e
Gergana Ivanova <geri7423@abv.bg>
2025-02-04 16:18:49 +0200

    Update SQL-IndicesAndDataAggregation.sql

--- SQL-IndicesAndDataAggregation.sql
+++ SQL-IndicesAndDataAggregation.sql
@@ -24,3 +24,10 @@ GROUP BY [DepositGroup]
     FROM [WizzardDeposits]
 GROUP BY [DepositGroup]
 ORDER BY AVG([MagicWandSize]) ASC
+
+--Task 5
+  SELECT [DepositGroup],
+         SUM([DepositAmount])
+      AS [TotalSum]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup]
