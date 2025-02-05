660df3fd810c69635f10637db113940749df484c
Gergana Ivanova <geri7423@abv.bg>
2025-02-05 12:46:57 +0200

    Update SQL-IndicesAndDataAggregation.sql

--- SQL-IndicesAndDataAggregation.sql
+++ SQL-IndicesAndDataAggregation.sql
@@ -201,3 +201,19 @@ GROUP BY [DepartmentID]
       AS [Count]
     FROM [Employees]
    WHERE [ManagerID] IS NULL
+
+--Task 18
+ SELECT [DepartmentID],
+        [Salary] 
+	 AS [ThirdHighestSalary]
+   FROM (
+             SELECT [DepartmentID],
+                    [Salary],
+			        DENSE_RANK() OVER(PARTITION BY [DepartmentID] ORDER BY [Salary] DESC)
+	             AS [SalaryRank]
+               FROM [Employees]
+         )
+      AS [TempTable]
+   WHERE [SalaryRank]= 3
+GROUP BY [DepartmentID],
+         [Salary]
