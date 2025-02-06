4475641afeb72e0b30fb21cf8ffcdd9144eb2a6d
Gergana Ivanova <geri7423@abv.bg>
2025-02-06 23:26:47 +0200

    Update SQL-FunctionsAndStoredProcedures.sql

---
+++ SQL-FunctionsAndStoredProcedures.sql
@@ -0,0 +1,11 @@
+USE [SoftUni]
+
+--Task 1
+CREATE PROCEDURE [usp_GetEmployeesSalaryAbove35000]
+AS
+  BEGIN
+	   SELECT [FirstName], 
+	          [LastName]
+         FROM [Employees]
+		WHERE [Salary] > 35000
+  END
