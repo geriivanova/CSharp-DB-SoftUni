7f6647731d49034d044ac659552b9bc3fc3b58f6
Gergana Ivanova <geri7423@abv.bg>
2025-02-08 22:52:37 +0200

    Update SQL-RegularExam–07 Aug 2024.sql

--- SQL-RegularExam–07 Aug 2024.sql
+++ SQL-RegularExam–07 Aug 2024.sql
@@ -96,3 +96,22 @@ WHERE [BrandId] IN (
 		               FROM [Brands]
 		              WHERE [Name] = 'Nike'
                    )
+
+--Task 4
+DELETE 
+  FROM [Orders]
+ WHERE [ShoeId] IN (
+                      SELECT [Id]  
+					    FROM [Shoes]
+					   WHERE [Model] = 'Joyride Run Flyknit'
+                   )
+DELETE 
+  FROM [ShoesSizes]
+ WHERE [ShoeId] IN (
+                      SELECT [Id]  
+					    FROM [Shoes]
+					   WHERE [Model] = 'Joyride Run Flyknit'
+                   )
+DELETE  
+  FROM [Shoes]
+ WHERE [Model] = 'Joyride Run Flyknit'
